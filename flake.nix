a3767eb825bd29962f2c78f6e4848bed3935b9f7
c04o <c04o@proton.me>
2026-01-07 21:21:31 -0600

    missing semicolon

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   }: {
     nixosConfigurations."c04o" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; }
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
       ];

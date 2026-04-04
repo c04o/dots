aa70fd94ef6d6cfd717f5d276dc44db3b531968a
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-04-03 23:28:36 -0600

    feat(flake): add zen-browser input
    also refactored a little

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   description = "my dots";
 
+  # define where the code is pulled from
   inputs = {
+    # Nix Packages collection & NixOS
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Community-driven Nix Flake for the Zen browser
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs = {
     self,
+    # defined above
     nixpkgs,
+    zen-browser,
     ...
   }: {
     nixosConfigurations."c04o" = nixpkgs.lib.nixosSystem {

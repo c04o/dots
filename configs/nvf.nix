db06ed3665ec892c244cc756042fd9614c94c22d
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-04-15 23:00:59 -0600

    chore(home): applied formatting
    run alejandra bc nvf.nix was missing formatting

--- configs/nvf.nix
+++ configs/nvf.nix
@@ -1,4 +1,8 @@
-{ pkgs, theme, ... }: {
+{
+  pkgs,
+  theme,
+  ...
+}: {
   programs.nvf = {
     enable = true;
     enableManpages = true;
@@ -58,58 +62,233 @@
 
         # language-specific
         languages = {
-          rust = { enable = true; crates.enable = true; lsp.enable = true; treesitter.enable = true; format.enable = true; };
-          go = { enable = true; lsp.enable = true; treesitter.enable = true; format.enable = true; };
-          clang = { enable = true; lsp.enable = true; treesitter.enable = true; };
-          zig = { enable = true; lsp.enable = true; treesitter.enable = true; };
-          lua = { enable = true; lsp.enable = true; treesitter.enable = true; format.enable = true; };
-          markdown = { enable = true; format.enable = true; lsp.enable = true; treesitter.enable = true; };
-          typst = { enable = true; format.enable = true; lsp.enable = true; treesitter.enable = true; };
-          json = { enable = true; treesitter.enable = true; lsp.enable = true; };
-          yaml = { enable = true; lsp.enable = true; treesitter.enable = true; };
+          rust = {
+            enable = true;
+            crates.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            format.enable = true;
+          };
+          go = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            format.enable = true;
+          };
+          clang = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          zig = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          lua = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            format.enable = true;
+          };
+          markdown = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          typst = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          json = {
+            enable = true;
+            treesitter.enable = true;
+            lsp.enable = true;
+          };
+          yaml = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
           toml.enable = true;
           xml.enable = true;
-          ts = { enable = true; format.enable = true; lsp.enable = true; treesitter.enable = true; };
-          svelte = { enable = true; lsp.enable = true; treesitter.enable = true; };
-          html = { enable = true; treesitter.enable = true; };
-          css = { enable = true; lsp.enable = true; treesitter.enable = true; };
+          ts = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          svelte = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          html = {
+            enable = true;
+            treesitter.enable = true;
+          };
+          css = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
           tailwind.enable = true;
-          python = { enable = true; lsp.enable = true; treesitter.enable = true; format.enable = true; };
-          nix = { enable = true; format.enable = true; lsp.enable = true; treesitter.enable = true; };
-          bash = { enable = true; format.enable = true; lsp.enable = true; treesitter.enable = true; };
+          python = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            format.enable = true;
+          };
+          nix = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          bash = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
           sql.enable = true;
         };
 
         # keymaps
         keymaps = [
           # picker
-          { key = "<leader>ff"; mode = "n"; silent = true; action = "<cmd>lua Snacks.picker.files()<CR>"; desc = "Find Files"; }
-          { key = "<leader>fg"; mode = "n"; silent = true; action = "<cmd>lua Snacks.picker.grep()<CR>"; desc = "Find Text"; }
-          { key = "<leader>fb"; mode = "n"; silent = true; action = "<cmd>lua Snacks.picker.buffers()<CR>"; desc = "Find Buffers"; }
- 
+          {
+            key = "<leader>ff";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.picker.files()<CR>";
+            desc = "Find Files";
+          }
+          {
+            key = "<leader>fg";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.picker.grep()<CR>";
+            desc = "Find Text";
+          }
+          {
+            key = "<leader>fb";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.picker.buffers()<CR>";
+            desc = "Find Buffers";
+          }
+
           # explorer
-          { key = "<leader>e"; mode = "n"; silent = true; action = "<cmd>lua Snacks.explorer()<CR>"; desc = "Toggle Explorer"; }
-         
+          {
+            key = "<leader>e";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.explorer()<CR>";
+            desc = "Toggle Explorer";
+          }
+
           # trouble (err list)
-          { key = "<leader>xx"; mode = "n"; silent = true; action = "<cmd>Trouble diagnostics toggle<CR>"; desc = "Toggle Error List"; }
-         
-           # gen
-          { key = "<leader>w"; mode = "n"; silent = true; action = "<cmd>w<CR>"; desc = "Save"; }
-          { key = "<leader>q"; mode = "n"; silent = true; action = "<cmd>q<CR>"; desc = "Quit"; }
-         
+          {
+            key = "<leader>xx";
+            mode = "n";
+            silent = true;
+            action = "<cmd>Trouble diagnostics toggle<CR>";
+            desc = "Toggle Error List";
+          }
+
+          # gen
+          {
+            key = "<leader>w";
+            mode = "n";
+            silent = true;
+            action = "<cmd>w<CR>";
+            desc = "Save";
+          }
+          {
+            key = "<leader>q";
+            mode = "n";
+            silent = true;
+            action = "<cmd>q<CR>";
+            desc = "Quit";
+          }
+
           # snacks git / utility
-          { key = "<leader>gg"; mode = "n"; silent = true; action = "<cmd>lua Snacks.lazygit()<CR>"; desc = "Open Lazygit"; }
-          { key = "<leader>gb"; mode = "n"; silent = true; action = "<cmd>lua Snacks.gitbrowse()<CR>"; desc = "Open in Browser"; }
-         
+          {
+            key = "<leader>gg";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.lazygit()<CR>";
+            desc = "Open Lazygit";
+          }
+          {
+            key = "<leader>gb";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.gitbrowse()<CR>";
+            desc = "Open in Browser";
+          }
+
           # snacks buffers & terminal
-          { key = "<leader>bd"; mode = "n"; silent = true; action = "<cmd>lua Snacks.bufdelete()<CR>"; desc = "Delete Buffer"; }
-          { key = "<leader>tt"; mode = "n"; silent = true; action = "<cmd>lua Snacks.terminal()<CR>"; desc = "Toggle Terminal"; }
-          { key = "<leader>z"; mode = "n"; silent = true; action = "<cmd>lua Snacks.zen()<CR>"; desc = "Toggle Zen Mode"; }
-          { key = "<leader>s"; mode = "n"; silent = true; action = "<cmd>lua Snacks.scratch()<CR>"; desc = "Toggle Scratch Buffer"; }
-          { key = "<leader>nh"; mode = "n"; silent = true; action = "<cmd>lua Snacks.notifier.show_history()<CR>"; desc = "Notification History"; }
-          { key = "<leader>rn"; mode = "n"; silent = true; action = "<cmd>lua Snacks.rename.rename_file()<CR>"; desc = "Rename File"; }
-          { key = "]]"; mode = ["n" "t"]; silent = true; action = "<cmd>lua Snacks.words.jump(1, true)<CR>"; desc = "Next LSP Reference"; }
-          { key = "[["; mode = ["n" "t"]; silent = true; action = "<cmd>lua Snacks.words.jump(-1, true)<CR>"; desc = "Prev LSP Reference"; }
+          {
+            key = "<leader>bd";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.bufdelete()<CR>";
+            desc = "Delete Buffer";
+          }
+          {
+            key = "<leader>tt";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.terminal()<CR>";
+            desc = "Toggle Terminal";
+          }
+          {
+            key = "<leader>z";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.zen()<CR>";
+            desc = "Toggle Zen Mode";
+          }
+          {
+            key = "<leader>s";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.scratch()<CR>";
+            desc = "Toggle Scratch Buffer";
+          }
+          {
+            key = "<leader>nh";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.notifier.show_history()<CR>";
+            desc = "Notification History";
+          }
+          {
+            key = "<leader>rn";
+            mode = "n";
+            silent = true;
+            action = "<cmd>lua Snacks.rename.rename_file()<CR>";
+            desc = "Rename File";
+          }
+          {
+            key = "]]";
+            mode = ["n" "t"];
+            silent = true;
+            action = "<cmd>lua Snacks.words.jump(1, true)<CR>";
+            desc = "Next LSP Reference";
+          }
+          {
+            key = "[[";
+            mode = ["n" "t"];
+            silent = true;
+            action = "<cmd>lua Snacks.words.jump(-1, true)<CR>";
+            desc = "Prev LSP Reference";
+          }
         ];
 
         # snacks plugins (from default.nix)
@@ -122,73 +301,73 @@
         luaConfigRC.snacks-setup = ''
           local snacks_config = vim.fn.json_decode('${builtins.toJSON {
             # Efficient animations inclduing over 45 easing functions
-            animate = {enabled = true;}; 
-            
+            animate = {enabled = true;};
+
             # Deal with big files
-            bigfile = {enabled = true;}; 
-            
+            bigfile = {enabled = true;};
+
             # Delete buffers without disrupting window layout
-            bufdelete = {enabled = true;}; 
-            
+            bufdelete = {enabled = true;};
+
             # Beautiful declarative dashboards
-            dashboard = {enabled = true;}; 
-            
+            dashboard = {enabled = true;};
+
             # Focus on the active scope by dimming the rest
-            dim = {enabled = true;}; 
-            
+            dim = {enabled = true;};
+
             # A file explorer
-            explorer = {enabled = true;}; 
-            
+            explorer = {enabled = true;};
+
             # Open the current file, branch, commit, or repo in a browser
-            gitbrowse = {enabled = true;}; 
-            
+            gitbrowse = {enabled = true;};
+
             # Image viewer
-            image = {enabled = true;}; 
-            
+            image = {enabled = true;};
+
             # Indent guides and scopes
-            indent = {enabled = true;}; 
-            
+            indent = {enabled = true;};
+
             # Better `vim.ui.input`
-            input = {enabled = true;}; 
-            
+            input = {enabled = true;};
+
             # Open LazyGit in a float
-            lazygit = {enabled = true;}; 
-            
+            lazygit = {enabled = true;};
+
             # Pretty `vim.notify`
-            notifier = {enabled = true;}; 
-            
+            notifier = {enabled = true;};
+
             # Picker for selecting items
-            picker = {enabled = true;}; 
-            
+            picker = {enabled = true;};
+
             # Faster file rendering
-            quickfile = {enabled = true;}; 
-            
+            quickfile = {enabled = true;};
+
             # LSP-integrated file renaming
-            rename = {enabled = true;}; 
-            
+            rename = {enabled = true;};
+
             # Scope detection
-            scope = {enabled = true;}; 
-            
+            scope = {enabled = true;};
+
             # Scratch buffers with a persistent file
-            scratch = {enabled = true;}; 
-            
+            scratch = {enabled = true;};
+
             # Smooth scrolling
-            scroll = {enabled = true;}; 
-            
+            scroll = {enabled = true;};
+
             # Status column
             statuscolumn = {enabled = true;};
-            
+
             # Create and toggle terminals
-            terminal = {enabled = true;}; 
-            
+            terminal = {enabled = true;};
+
             # Toggle keymaps integrated with which-key Icons / colors
-            toggle = {enabled = true;}; 
-            
+            toggle = {enabled = true;};
+
             # Auto-show LSP references and navigate them
-            words = {enabled = true;}; 
-            
+            words = {enabled = true;};
+
             # Zen mode • distraction-free coding
-            zen = {enabled = true;}; 
+            zen = {enabled = true;};
           }}')
 
           require('snacks').setup(snacks_config)

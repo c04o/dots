5e7227ff5e07c3e0955610ed26cc8f2f14c86a6e
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-06-07 18:31:23 -0600

    fix(git): remove home-manager block
    NOOOOOOOO

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,30 +1,27 @@
 {pkgs, ...}: {
-  # system-wide git requirements
-  home-manager.users.coni = {
-    programs.git = {
-      enable = true;
-      settings = {
-        user = {
-          name = "Connie Caldera";
-          email = "166080234+c04o@users.noreply.github.com";
-        };
-        gpg = {
-          format = "openpgp";
-          program = "gpg";
-        };
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Connie Caldera";
+        email = "166080234+c04o@users.noreply.github.com";
       };
-      signing = {
-        key = "BE9C532A39E47670";
-        signByDefault = true;
+      gpg = {
+        format = "openpgp";
+        program = "gpg";
       };
     };
-
-    home.packages = with pkgs; [
-      # simple terminal UI for git commands
-      lazygit
-
-      # Modern release of the GNU Privacy Guard, a GPL OpenPGP implementation
-      gnupg
-    ];
+    signing = {
+      key = "BE9C532A39E47670";
+      signByDefault = true;
+    };
   };
+
+  home.packages = with pkgs; [
+    # simple terminal UI for git commands
+    lazygit
+
+    # Modern release of the GNU Privacy Guard, a GPL OpenPGP implementation
+    gnupg
+  ];
 }

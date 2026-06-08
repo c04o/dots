0ccab92f3d446db370f165a3f07b09dba814a5c4
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-06-07 18:27:33 -0600

    fix(nvf): delete `home-manger.users.coni` block
    since it's imported with flakes already

--- modules/dev/nvf/default.nix
+++ modules/dev/nvf/default.nix
@@ -7,49 +7,47 @@
   imports = [
     inputs.nvf.homeManagerModules.default
   ];
-  home-manager.users.coni = {
-    programs.nvf = {
-      enable = true;
-      enableManpages = true;
+  programs.nvf = {
+    enable = true;
+    enableManpages = true;
 
-      settings.vim = {
-        viAlias = true;
-        vimAlias = true;
+    settings.vim = {
+      viAlias = true;
+      vimAlias = true;
 
-        # UI & QoL basic setups
-        binds.whichKey.enable = true;
-        autopairs.nvim-autopairs.enable = true;
-        statusline.lualine.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        git.gitsigns.enable = true;
+      # UI & QoL basic setups
+      binds.whichKey.enable = true;
+      autopairs.nvim-autopairs.enable = true;
+      statusline.lualine.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+      git.gitsigns.enable = true;
 
-        # Core options
-        options = {
-          termguicolors = true;
-          tabstop = 2;
-          shiftwidth = 2;
-          expandtab = true;
-          number = true;
-          relativenumber = true;
-          cursorline = true;
-        };
+      # Core options
+      options = {
+        termguicolors = true;
+        tabstop = 2;
+        shiftwidth = 2;
+        expandtab = true;
+        number = true;
+        relativenumber = true;
+        cursorline = true;
+      };
 
-        # Dynamic theme matching
-        theme = {
-          enable = true;
-          name = theme.theme.name;
-          style = theme.theme.style;
-        };
+      # Dynamic theme matching
+      theme = {
+        enable = true;
+        name = theme.theme.name;
+        style = theme.theme.style;
+      };
 
-        # Global variable space
-        globals = {
-          mapleader = " ";
-          everforest_background = theme.theme.style; # hard, medium, soft
-          everforest_transparent_background = 0; # 0: opaque, 1: transparent
-          everforest_better_performance = 1;
-          everforest_enable_italic = 1; # 0: disabled, 1: enabled
-          everforest_diagnostic_text_highlight = 1;
-        };
+      # Global variable space
+      globals = {
+        mapleader = " ";
+        everforest_background = theme.theme.style; # hard, medium, soft
+        everforest_transparent_background = 0; # 0: opaque, 1: transparent
+        everforest_better_performance = 1;
+        everforest_enable_italic = 1; # 0: disabled, 1: enabled
+        everforest_diagnostic_text_highlight = 1;
       };
     };
   };

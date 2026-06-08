e09bd456cfd9529cf5ebf577baf32af79ae87e9a
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-06-07 18:23:16 -0600

    fix(git): unnest `config.flake.modules`
    that wrap is causing err

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,32 +1,30 @@
 {pkgs, ...}: {
-  config.flake.modules = {
-    # system-wide git requirements
-    home-manager.users.coni = {
-      programs.git = {
-        enable = true;
-        settings = {
-          user = {
-            name = "Connie Caldera";
-            email = "166080234+c04o@users.noreply.github.com";
-          };
-          gpg = {
-            format = "openpgp";
-            program = "gpg";
-          };
+  # system-wide git requirements
+  home-manager.users.coni = {
+    programs.git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Connie Caldera";
+          email = "166080234+c04o@users.noreply.github.com";
         };
-        signing = {
-          key = "BE9C532A39E47670";
-          signByDefault = true;
+        gpg = {
+          format = "openpgp";
+          program = "gpg";
         };
       };
-
-      home.packages = with pkgs; [
-        # simple terminal UI for git commands
-        lazygit
-
-        # Modern release of the GNU Privacy Guard, a GPL OpenPGP implementation
-        gnupg
-      ];
+      signing = {
+        key = "BE9C532A39E47670";
+        signByDefault = true;
+      };
     };
+
+    home.packages = with pkgs; [
+      # simple terminal UI for git commands
+      lazygit
+
+      # Modern release of the GNU Privacy Guard, a GPL OpenPGP implementation
+      gnupg
+    ];
   };
 }

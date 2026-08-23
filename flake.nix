01bbdf0a4cf2a647c215fd6f6deaddffebe5905d
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-08-23 15:13:15 -0600

    feat(flake): add `stable` dms input
    won't work otherwise

--- flake.nix
+++ flake.nix
@@ -23,6 +23,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # get dms plugins
     dms-plugin-registry = {
       url = "github:AvengeMedia/dms-plugin-registry";
